adc3c1438be5b75cb0f6f5ed7a379d7ed91a01b4
ckacquah <49785034+ckacquah@users.noreply.github.com>
2022-07-13 16:02:51 +0000

    Adding seeder for testing parts of the application

--- backend/img-processor/app/seeders.py
+++ backend/img-processor/app/seeders.py
@@ -1,21 +1,35 @@
+import logging
 from faker import Faker
 
+from app.utils import generate_random_file_name
 from app.base_model import db
-from app.modules.image.models import File
+from app.modules.image.models import Image
 
 fake = Faker()
 
+logger = logging.getLogger("seeder")
+
+
+def run_image_seeder():
+    logger.info("Running image seeder...")
+    # create 5 images
+    images = []
+    for i in range(5):
+        image = Image(
+            name=fake.name(),
+            size=fake.random_int(min=1, max=100),
+            width=fake.random_int(min=1, max=100),
+            height=fake.random_int(min=1, max=100),
+            source="upload",
+            storage_name=generate_random_file_name(),
+        )
+        images.append(image)
+        logger.info(f"{image} created")
+    db.session.add_all(images)
+    db.session.commit()
+    logger.info("Image seeder has completed")
+
 
 def run_seeds():
-    # create 5 files
-    # files = []
-    # for i in range(5):
-    #     file = File(
-    #         name=fake.name(),
-    #         age=fake.random_int(min=1, max=100)
-    #     )
-    #     files.append(file)
-    #     print(f"{file} created")
-    # db.session.add_all(files)
-    # db.session.commit()
-    print("File Seeds written to database")
+    logger.info("Start seeding...")
+    run_image_seeder()
