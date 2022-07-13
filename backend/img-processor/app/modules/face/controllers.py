cb3ed214226d0f84b75c4e406cc1d899755a3f32
ckacquah <49785034+ckacquah@users.noreply.github.com>
2022-07-13 23:09:01 +0000

    Add & Delete faces

--- backend/img-processor/app/modules/face/controllers.py
+++ backend/img-processor/app/modules/face/controllers.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 from app.base_model import db
 from app.modules.face.models import Face
-from app.modules.face.schemas import faces_schema
+from app.modules.face.schemas import faces_schema, face_schema
 
 face_controller = Blueprint("face", __name__, url_prefix="/face")
 
@@ -10,3 +10,21 @@ face_controller = Blueprint("face", __name__, url_prefix="/face")
 @face_controller.route("/", methods=["GET"])
 def get_all_faces():
     return jsonify(faces_schema.dump(Face.query.all())), 200
+
+
+@face_controller.route("/<face_id>", methods=["GET"])
+def get_face_by_id(face_id):
+    face = Face.query.get(face_id)
+    if face is None:
+        return jsonify({"message": "Face not found"}), 404
+    return jsonify(face_schema.dump(face)), 200
+
+
+@face_controller.route("/<face_id>", methods=["DELETE"])
+def delete_face_by_id(face_id):
+    face = Face.query.get(face_id)
+    if face is None:
+        return jsonify({"message": "Face not found"}), 404
+    db.session.delete(face)
+    db.session.commit()
+    return jsonify({"message": "Face has been deleted successfully"}), 200
--- backend/img-processor/test/modules/face/controller_test.py
+++ backend/img-processor/test/modules/face/controller_test.py
@@ -25,3 +25,32 @@ def test_get_all_faces_returns_nothing_if_db_is_empty(client):
     response = client.get("face/")
     assert response.status_code == 200
     assert len(response.json) == 0
+
+
+def test_get_face_by_id(client):
+    run_face_seeder()
+    for face in Face.query.all():
+        response = client.get(f"face/{face.uuid}")
+        assert response.status_code == 200
+        assert face_schema.dump(face) == response.json
+
+
+def test_get_face_by_id_returns_404_if_face_does_not_exist(client):
+    response = client.get(f"face/{str(uuid.uuid4())}")
+    assert response.status_code == 404
+    assert response.json["message"] == "Face not found"
+
+
+def test_delete_face_by_id(client):
+    run_face_seeder()
+    for index, face in enumerate(Face.query.all()):
+        response = client.delete(f"face/{str(face.uuid)}")
+        assert response.status_code == 200
+        assert len(Face.query.all()) == 5 - index - 1
+        assert response.json["message"] == "Face has been deleted successfully"
+
+
+def test_delete_face_by_id_returns_404_if_face_does_not_exist(client):
+    response = client.delete(f"face/{str(uuid.uuid4())}")
+    assert response.status_code == 404
+    assert response.json["message"] == "Face not found"
