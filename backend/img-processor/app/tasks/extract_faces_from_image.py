a70fa4b6e9c7bd27fdca25718e5ad14ab43370ef
ckacquah <49785034+ckacquah@users.noreply.github.com>
2022-07-24 18:47:20 +0000

    Improving face extraction feature & code refactor

--- backend/img-processor/app/tasks/extract_faces_from_image.py
+++ backend/img-processor/app/tasks/extract_faces_from_image.py
@@ -1,77 +1,21 @@
-import os
-import cv2
-from retinaface import RetinaFace
-
 from app.tasks import celery
-from app.base_model import db
-from app.modules.face.models import Face
-from app.modules.face.schemas import faces_schema
-from app.modules.image.models import Image
-from app.utils import (
-    get_uploaded_file_path,
-    get_processed_face_path,
-    generate_random_filename,
+from app.utils import get_uploaded_file_path
+from app.modules.image.services import (
+    detect_faces_from_image,
+    store_detected_faces_to_db,
+    store_detected_faces_images_to_processed_folder,
 )
 
 
-@celery.task(name="extract-faces-from-image", bind=True)
-def extract_faces_from_image(self, image_param):
-    image_file = Image.query.get(image_param["image"])
-    image_path = get_uploaded_file_path(image_file.storage_name)
-    detected_faces = RetinaFace.detect_faces(image_path).values()
-    extracted_faces = extract_faces_as_images(image_path, detected_faces)
-    saved_faces = save_extracted_faces_to_storage(extracted_faces)
-    faces = save_extracted_faces_to_db(saved_faces, image_file)
-    return {"faces": faces}
-
-
-def extract_faces_as_images(image_path, detected_faces):
-    extracted_faces = []
-    image = cv2.imread(image_path)
-    for detected_face in detected_faces:
-        x1, y1, x2, y2 = detected_face["facial_area"]
-        extracted_faces.append(
-            {
-                "image": image[y1:y2, x1:x2],
-                "score": detected_face["score"],
-                "facial_area": detected_face["facial_area"],
-            }
-        )
-    return extracted_faces
-
-
-def save_extracted_faces_to_storage(extracted_faces):
-    saved_faces = []
-    for extracted_face in extracted_faces:
-        file_name = generate_random_filename() + ".jpeg"
-        file_path = get_processed_face_path(file_name)
-        cv2.imwrite(file_path, extracted_face["image"])
-        saved_faces.append(
-            {
-                "file_name": file_name,
-                "score": extracted_face["score"],
-                "facial_area": extracted_face["facial_area"],
-            }
-        )
-    return saved_faces
-
-
-def save_extracted_faces_to_db(saved_faces, parent):
-    images, faces = [], []
-    for face in saved_faces:
-        x1, y1, x2, y2 = face["facial_area"]
-        image_path = get_processed_face_path(face["file_name"])
-        images.append(
-            Image(
-                name=face["file_name"],
-                storage_name=face["file_name"],
-                source="processed",
-                height=y2 - y1,
-                width=x2 - x1,
-                size=os.path.getsize(image_path),
-            )
-        )
-        faces.append(Face(file=images[-1], parent=parent, score=face["score"]))
-    db.session.add_all(images + faces)
-    db.session.commit()
-    return faces_schema.dump(faces)
+@celery.task(name="extract-faces-from-image")
+def extract_faces_from_image(image):
+    image_path = get_uploaded_file_path(image.storage_name)
+    detected_faces = detect_faces_from_image(image_path)
+    stored_detected_faces = store_detected_faces_images_to_processed_folder(
+        detected_faces
+    )
+    stored_detected_faces = store_detected_faces_to_db(
+        stored_detected_faces,
+        parent=image,
+    )
+    return stored_detected_faces
--- backend/img-processor/tests/unit/test_extract_faces_from_image.py
+++ backend/img-processor/tests/unit/test_extract_faces_from_image.py
@@ -1,107 +1,298 @@
-import os
-import cv2
+import PIL
+from unittest.mock import Mock, call
 
+from app.tasks.extract_faces_from_image import extract_faces_from_image
+from app.seeders import run_image_seeder
 from app.modules.face.models import Face
 from app.modules.image.models import Image
-from app.utils import get_processed_face_path
-from app.utils.testing import upload_image, get_sample_image_path
-from app.tasks.extract_faces_from_image import (
-    extract_faces_as_images,
-    extract_faces_from_image,
-    save_extracted_faces_to_db,
-    save_extracted_faces_to_storage,
+from app.modules.image.services import (
+    detect_faces_from_image,
+    store_detected_faces_to_db,
+    store_processed_image_info_to_db,
+    store_detected_faces_image_info_to_db,
+    store_PIL_Image_as_jpeg_in_processed_folder,
+    store_detected_faces_images_to_processed_folder,
+)
+from app.utils.testing import (
+    get_sample_image_path,
+    no_image_pixel_differences,
 )
 
+SAMPLE_IMAGE_PATH = get_sample_image_path("sample.jpg")
 
-SAMPLE_FACES = [
-    {
-        "score": 0.9996384382247925,
-        "facial_area": [353, 25, 387, 72],
-    },
-    {
-        "score": 0.9996119141578674,
-        "facial_area": [109, 65, 148, 112],
-    },
-    {
+SAMPLE_PIL_IMAGE = PIL.Image.open(SAMPLE_IMAGE_PATH)
+
+PROCESSED_IMAGE_INFO = {
+    "size": 666,
+    "width": 666,
+    "height": 666,
+    "file_name": "file.jpg",
+}
+
+SAMPLE_FACES = {
+    "face_1": {
         "score": 0.9995879530906677,
         "facial_area": [456, 30, 495, 80],
     },
-    {
-        "score": 0.999541163444519,
+    "face_2": {
+        "score": 0.9995411634445019,
         "facial_area": [261, 58, 297, 107],
     },
-]
+}
 
 
-def test_extract_faces_from_image_task(client):
-    upload_image(client, "sample.jpg")
+def test_extract_faces_from_image_object(client, monkeypatch):
+    """
+    GIVEN an Image object
+    WHEN faces are extracted from the image
+    THEN check that faces are extracted and stored correctly
+    """
+    run_image_seeder()
     image = Image.query.first()
-    results = extract_faces_from_image.apply(
-        args=(
-            {
-                "image": image.uuid,
-                "backend": "opencv",
-            },
-        ),
-    ).get()
-    assert results is not None
-    assert results["faces"] is not None
-    assert len(results["faces"]) == 5
-    assert len(Image.query.all()) == 6
-    assert len(Face.query.all()) == 5
-    for face in Face.query.all():
-        assert face.file is not None
-        assert face.parent.uuid == image.uuid
 
+    mock_get_uploaded_file_path = Mock(return_value=SAMPLE_IMAGE_PATH)
+    mock_detect_faces_from_image = Mock(return_value=SAMPLE_FACES)
+    mock_store_detected_faces_to_db = Mock(return_value={})
+    mock_store_detected_faces_images_to_disk = Mock(return_value=SAMPLE_FACES)
 
-def test_extract_faces_as_images(client):
-    extract_faces = extract_faces_as_images(
-        get_sample_image_path("sample.jpg"), SAMPLE_FACES
+    monkeypatch.setattr(
+        "app.tasks.extract_faces_from_image.get_uploaded_file_path",
+        mock_get_uploaded_file_path,
     )
-    assert extract_faces is not None
-    assert len(extract_faces) == 4
-    for index, extract_face in enumerate(extract_faces):
-        assert extract_face["score"] == SAMPLE_FACES[index]["score"]
-        assert (
-            extract_face["facial_area"] == SAMPLE_FACES[index]["facial_area"]
+    monkeypatch.setattr(
+        "app.tasks.extract_faces_from_image.detect_faces_from_image",
+        mock_detect_faces_from_image,
+    )
+    monkeypatch.setattr(
+        "app.tasks.extract_faces_from_image.store_detected_faces_to_db",
+        mock_store_detected_faces_to_db,
+    )
+    monkeypatch.setattr(
+        "app.tasks.extract_faces_from_image."
+        "store_detected_faces_images_to_processed_folder",
+        mock_store_detected_faces_images_to_disk,
+    )
+
+    faces = extract_faces_from_image.apply(args=(image,)).get()
+
+    mock_get_uploaded_file_path.assert_called_once_with(image.storage_name)
+    mock_detect_faces_from_image.assert_called_once_with(SAMPLE_IMAGE_PATH)
+    mock_store_detected_faces_to_db.assert_called_once_with(
+        SAMPLE_FACES, parent=image
+    )
+    mock_store_detected_faces_images_to_disk.assert_called_once_with(
+        SAMPLE_FACES
+    )
+
+    assert faces == {}
+
+
+def test_store_PIL_Image_as_jpeg_in_processed_folder(client, monkeypatch):
+    """
+    GIVEN a PIL.Image.Image object
+    WHEN the image file is stored in the processed folder
+    THEN check that the file is stored correctly and returns information about
+         the processed image
+    """
+    filename = "01.jpeg"
+    mock_generate_random_filename = Mock(return_value=filename)
+    mock_os_path_getsize = Mock(return_value=666)
+
+    monkeypatch.setattr(
+        "app.modules.image.services.generate_random_filename",
+        mock_generate_random_filename,
+    )
+    monkeypatch.setattr(
+        "app.modules.image.services.os.path.getsize",
+        mock_os_path_getsize,
+    )
+
+    file_path = get_sample_image_path(filename)
+    test_image = PIL.Image.open(file_path)
+    processed_image_info = store_PIL_Image_as_jpeg_in_processed_folder(
+        test_image
+    )
+    processed_image = PIL.Image.open(processed_image_info["file_path"])
+
+    assert processed_image.format == "JPEG"
+    assert processed_image_info["size"] == 666
+    assert processed_image_info["width"] == 259
+    assert processed_image_info["height"] == 194
+    assert processed_image_info["file_name"] == filename
+    assert no_image_pixel_differences(processed_image, test_image)
+
+    mock_generate_random_filename.assert_called_once_with(extension="jpg")
+    mock_os_path_getsize.assert_called_once_with(
+        processed_image_info["file_path"]
+    )
+
+
+def test_store_processed_image_info_to_db(client):
+    """
+    GIVEN the processed image info
+    WHEN the processed image info is stored to the database
+    THEN check that the file info is stored correctly and return the image
+         model object
+    """
+    image = store_processed_image_info_to_db(PROCESSED_IMAGE_INFO)
+    assert image == Image.query.first()
+    assert image.size == PROCESSED_IMAGE_INFO["size"]
+    assert image.width == PROCESSED_IMAGE_INFO["width"]
+    assert image.height == PROCESSED_IMAGE_INFO["height"]
+    assert image.name == PROCESSED_IMAGE_INFO["file_name"]
+    assert image.storage_name == PROCESSED_IMAGE_INFO["file_name"]
+
+
+def test_detect_faces_from_image(client, monkeypatch):
+    """
+    GIVEN the image path
+    WHEN faces are detected from the image
+    THEN check that faces are detected correctly and return the image
+         info about the faces
+    """
+
+    mock_PIL_Image_crop = Mock(return_value="crop")
+    mock_PIL_Image_open = Mock(return_value=SAMPLE_PIL_IMAGE)
+    mock_RetinaFace_detect_faces = Mock(return_value=SAMPLE_FACES)
+
+    monkeypatch.setattr(
+        "app.modules.image.services.RetinaFace.detect_faces",
+        mock_RetinaFace_detect_faces,
+    )
+    monkeypatch.setattr(
+        "app.modules.image.services.PIL.Image.open",
+        mock_PIL_Image_open,
+    )
+    monkeypatch.setattr(
+        "app.modules.image.services.PIL.Image.Image.crop",
+        mock_PIL_Image_crop,
+    )
+
+    detected_faces = detect_faces_from_image(SAMPLE_IMAGE_PATH)
+
+    mock_PIL_Image_crop.assert_has_calls(
+        [
+            call(tuple(SAMPLE_FACES["face_1"]["facial_area"])),
+            call(tuple(SAMPLE_FACES["face_2"]["facial_area"])),
+        ]
+    )
+    mock_PIL_Image_open.assert_called_once_with(SAMPLE_IMAGE_PATH)
+    mock_RetinaFace_detect_faces.assert_called_with(SAMPLE_IMAGE_PATH)
+
+    for face_id, face_info in detected_faces.items():
+        assert face_info["PIL_image"] == "crop"
+        assert face_info["score"] == SAMPLE_FACES[face_id]["score"]
+        assert face_info["facial_area"] == SAMPLE_FACES[face_id]["facial_area"]
+
+
+def test_store_detected_faces_images_to_processed_folder(client, monkeypatch):
+    """
+    GIVEN the detected faces dict
+    WHEN the object is stored to the processed folder
+    THEN check that faces are stored correctly and return info about the faces
+    """
+    sample_detected_faces = SAMPLE_FACES.copy()
+
+    for face_id, face_info in sample_detected_faces.items():
+        sample_detected_faces[face_id].update({"PIL_image": SAMPLE_PIL_IMAGE})
+
+    mock_store_PIL_Image_as_jpeg_in_processed_folder = Mock(
+        return_value=PROCESSED_IMAGE_INFO
+    )
+
+    monkeypatch.setattr(
+        "app.modules.image.services."
+        "store_PIL_Image_as_jpeg_in_processed_folder",
+        mock_store_PIL_Image_as_jpeg_in_processed_folder,
+    )
+
+    faces = store_detected_faces_images_to_processed_folder(
+        sample_detected_faces
+    )
+
+    mock_store_PIL_Image_as_jpeg_in_processed_folder.assert_called_with(
+        SAMPLE_PIL_IMAGE
+    )
+
+    for face_id, face_info in faces.items():
+        assert face_info["score"] == SAMPLE_FACES[face_id]["score"]
+        assert face_info["facial_area"] == SAMPLE_FACES[face_id]["facial_area"]
+        assert face_info["image_info"] == PROCESSED_IMAGE_INFO
+
+
+def test_store_detected_faces_image_info_to_db(client, monkeypatch):
+    """
+    GIVEN the detected faces dict
+    WHEN the object is stored to the database
+    THEN check that images info are stored correctly
+    """
+    sample_detected_faces = SAMPLE_FACES.copy()
+
+    for face_id, face_info in sample_detected_faces.items():
+        sample_detected_faces[face_id].update(
+            {"image_info": PROCESSED_IMAGE_INFO}
         )
-        facial_area = SAMPLE_FACES[index]["facial_area"]
-        image_height, image_width, image_channels = extract_face["image"].shape
-        assert image_channels == 3
-        assert image_width == facial_area[2] - facial_area[0]
-        assert image_height == facial_area[3] - facial_area[1]
 
+    mock_store_processed_image_info_to_db = Mock(return_value="image")
 
-def test_save_extracted_faces_to_storage(client):
-    extracted_faces = extract_faces_as_images(
-        get_sample_image_path("sample.jpg"), SAMPLE_FACES
+    monkeypatch.setattr(
+        "app.modules.image.services.store_processed_image_info_to_db",
+        mock_store_processed_image_info_to_db,
     )
-    saved_faces = save_extracted_faces_to_storage(extracted_faces)
-    for index, saved_face in enumerate(saved_faces):
-        assert os.path.exists(get_processed_face_path(saved_face["file_name"]))
-        assert saved_face["score"] == SAMPLE_FACES[index]["score"]
-        assert saved_face["facial_area"] == SAMPLE_FACES[index]["facial_area"]
-        img = cv2.imread(get_processed_face_path(saved_face["file_name"]))
-        facial_area = SAMPLE_FACES[index]["facial_area"]
-        image_height, image_width, image_channels = img.shape
-        assert image_channels == 3
-        assert image_width == facial_area[2] - facial_area[0]
-        assert image_height == facial_area[3] - facial_area[1]
 
+    faces = store_detected_faces_image_info_to_db(sample_detected_faces)
 
-def test_save_extracted_faces_to_db(client):
-    upload_image(client, "sample.jpg")
-    image = Image.query.filter_by(name="sample.jpg").first()
-    extracted_faces = extract_faces_as_images(
-        get_sample_image_path("sample.jpg"), SAMPLE_FACES
+    mock_store_processed_image_info_to_db.assert_called_with(
+        PROCESSED_IMAGE_INFO
     )
-    saved_faces = save_extracted_faces_to_storage(extracted_faces)
-    save_extracted_faces_to_db(saved_faces, parent=image)
-    assert len(Image.query.all()) == 5
-    assert len(Face.query.all()) == 4
-    for face in Face.query.all():
-        assert face.file is not None
-        assert face.parent is not None
-        assert Image.query.get(face.file.uuid) is not None
-        assert Image.query.get(face.parent.uuid) is not None
-        assert face.parent.uuid == image.uuid
+
+    for face_id, face_info in faces.items():
+        assert face_info["image"] == "image"
+        assert face_info["score"] == SAMPLE_FACES[face_id]["score"]
+        assert face_info["facial_area"] == SAMPLE_FACES[face_id]["facial_area"]
+        assert face_info["image_info"] == PROCESSED_IMAGE_INFO
+
+
+def test_store_detected_faces_to_db(client, monkeypatch):
+    """
+    GIVEN the detected faces dict
+    WHEN the object is stored to the database
+    THEN check that face info are stored correctly and returns a list of face
+         dicts
+    """
+    run_image_seeder()
+
+    images = Image.query.all()
+    face_image = images[0]
+    parent_image = images[1]
+
+    sample_detected_faces = SAMPLE_FACES.copy()
+
+    for face_id, face_info in sample_detected_faces.items():
+        sample_detected_faces[face_id].update({"image": face_image})
+
+    faces = store_detected_faces_to_db(sample_detected_faces, parent_image)
+
+    assert len(Face.query.all()) == 2
+    for face in faces:
+        face_dao = Face.query.get(face["uuid"])
+        sample_face_dict = None
+        for sample_face in sample_detected_faces.values():
+            if sample_face["score"] == face_dao.score:
+                sample_face_dict = sample_face
+
+        assert face_dao.uuid == face["uuid"]
+        assert face_dao.score == face["score"]
+        assert face_dao.uuid == face["uuid"]
+        assert face_dao.file_uuid == face["file_uuid"]
+        assert face_dao.parent_uuid == face["parent_uuid"]
+        assert face_dao.facial_area.x1 == face["facial_area"]["x1"]
+        assert face_dao.facial_area.y1 == face["facial_area"]["y1"]
+        assert face_dao.facial_area.x2 == face["facial_area"]["x2"]
+        assert face_dao.facial_area.y2 == face["facial_area"]["y2"]
+        assert face_dao.file == face_image
+        assert face_dao.parent == parent_image
+        assert face_dao.facial_area.x1 == sample_face_dict["facial_area"][0]
+        assert face_dao.facial_area.y1 == sample_face_dict["facial_area"][1]
+        assert face_dao.facial_area.x2 == sample_face_dict["facial_area"][2]
+        assert face_dao.facial_area.y2 == sample_face_dict["facial_area"][3]
