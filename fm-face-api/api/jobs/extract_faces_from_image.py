3836963f69e77463f8207d3003070588702c97b1
ckacquah <49785034+ckacquah@users.noreply.github.com>
2022-07-30 10:34:22 +0000

    refactor: Trying to use function pipeline to clean code

--- fm-face-api/api/jobs/extract_faces_from_image.py
+++ fm-face-api/api/jobs/extract_faces_from_image.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from api.jobs import celery
 from api.utils import get_uploaded_file_path
 from api.modules.image.models import Image
@@ -9,15 +11,22 @@ from api.modules.image.services import (
 )
 
 
-@celery.task(name="extract-faces-from-image")
+def execute_face_extraction_pipeline(image_storage_name):
+    return reduce(
+        lambda value, function: function(value),
+        (
+            get_uploaded_file_path,
+            detect_faces_from_image,
+            store_detected_faces_image_info_to_db,
+            store_detected_faces_images_to_processed_folder,
+        ),
+        image_storage_name,
+    )
+
+
+@celery.task(name="job-face-extraction")
 def extract_faces_from_image(image_id):
     image = Image.query.get(image_id)
-    image_path = get_uploaded_file_path(image.storage_name)
-    faces = detect_faces_from_image(image_path)
-    faces = store_detected_faces_images_to_processed_folder(faces)
-    faces = store_detected_faces_image_info_to_db(faces)
-    faces = store_detected_faces_to_db(
-        faces,
-        parent=image,
-    )
-    return faces
+    faces = execute_face_extraction_pipeline(image.storage_name)
+    results = store_detected_faces_to_db(faces, parent=image)
+    return results
